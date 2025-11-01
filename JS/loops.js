7b77d12dd6bf9f9dd6e6a87a206bb6f0c4adc00e
mansi-nirwal <manshinirwal1989@gmail.com>
2025-11-01 21:13:22 +0530

    updated

---
+++ JS/loops.js
@@ -0,0 +1,25 @@
+// for(let i = 1; i<=10; i++){
+//     console.log(i)
+// }
+
+// for(let i = 1 ; i<=20; i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+//  for(let i = 10 ; i >= 1; i--){
+//     console.log(i)
+//  }
+
+//  for(let i = 0;i<5 ; i++){
+//     console.log("Yes")
+//  }
+
+// let num = prompt("Enter Number");
+// if(num%2==0){
+//     alert("Even Number");
+// }
+// else{
+//     alert("Odd Number")
+// }
