4b3665f10468ad041217c83ccebc98f9ad39719c
mansi-nirwal <manshinirwal1989@gmail.com>
2025-11-07 11:04:44 +0530

    updated

---
+++ JS/operator.js
@@ -0,0 +1,90 @@
+// Arithmetic operators
+
+// + - * / % **
+
+let a = 20;
+
+// Assignments Operators
+
+// = += -= *= /= %=
+
+a += 3;
+
+a -= 3;
+
+a *= 3;
+
+a /= 3;
+
+a %= 4;
+
+//
+
+console.log(a);
+
+// Comparison Operators
+
+// == === != !== < > >= <-
+
+// == not strict compare Never use this
+
+// === strict compare Always Use This
+
+// != Never Use This
+
+// !== Always use this
+
+// > greater
+
+// > smaller
+
+// >= greater equal
+
+// <= less equal
+
+// Logical Operators
+
+// && || !
+
+// Ternary Operator
+
+12 > 13 ? console.log("hey ") : console.log("hell");
+
+let num = 10;
+let num2 = 3;
+
+let res;
+
+res = num + num2;
+
+res = num - num2;
+
+res = num * num2;
+
+res = num / num2;
+
+res = num % num2;
+
+console.log(res);
+
+let x = 5;
+
+//  x = x + 3;
+
+x += 5;
+
+console.log(x);
+
+let count = 5;
+count++;
+count--;
+console.log(count);
+
+
+let v1 = 5;
+
+let v2 = "5";
+
+console.log(v1===v2);
+
+
