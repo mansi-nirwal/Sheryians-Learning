e0dc718362abf9fdd9ee8056ed58d99fc76d6f63
mansi-nirwal <manshinirwal1989@gmail.com>
2025-11-17 15:53:58 +0530

    loops questions

--- JS/loops.js
+++ JS/loops.js
@@ -1,25 +1,55 @@
-// for(let i = 1; i<=10; i++){
+// for(let i = 1 ; i <= 10; i++){
 //     console.log(i)
 // }
 
-// for(let i = 1 ; i<=20; i++){
-//     if(i%2==0){
+// for(let i = 1; i<=20; i++){
+//     if(i%2===0){
 //         console.log(i)
 //     }
 // }
 
-//  for(let i = 10 ; i >= 1; i--){
+// for(let i = 10; i >= 1; --i){
 //     console.log(i)
-//  }
+// }
 
-//  for(let i = 0;i<5 ; i++){
-//     console.log("Yes")
-//  }
+// for(let i = 1; i<=5; i++){
+//     console.log('hello')
+// }
 
-// let num = prompt("Enter Number");
-// if(num%2==0){
-//     alert("Even Number");
+// for(let i = 0; i<=10; i++){
+//     if(i%2 === 0){
+//         console.log(`${i} Even`)
+//     }
+//     else{
+//         console.log(`${i} - Odd`)
+//     }
+// }
+
+// promt se jab bhi kuch lete hai wo string me deta hai string se number me convert krne ke teen tarike hai 
+// parseInt()
+// Numver()
+// + 
+// let num = +prompt("Enter a number");
+
+// let num1 = prompt("Number");
+// num1 = parseInt(num);
+
+// if(num1>=0){
+//     console.log("Positive")
 // }
 // else{
-//     alert("Odd Number")
+//     console.log("Negative")
+// }
+
+
+// let age = prompt("Enter age");
+
+// if(age===null){
+//     console.error("You cancelled it ")
+// }
+// else{
+//     if(age.trim()=== ""){
+//         console.error("dhang se")
+//     }
+
 // }
